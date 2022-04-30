787b0ee573efc81a48c400c76aed24627d78e0e0
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-04-30 04:26:41 -0700

    index added

--- src/components/Projects/SmallProjects/ProjectCards.js
+++ src/components/Projects/SmallProjects/ProjectCards.js
@@ -22,48 +22,50 @@ const ProjectCards = () => {
         >
             {ProjectList.map((project, index) => {
                 return (
-                    <Grow
-                        in={true}
-                        timeout={2000}
-                        style={{
-                            transitionDelay: project.delay,
-                        }}
-                    >
-                        <Card sx={{ maxWidth: 300, mb: 1 }}>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image={project.image}
-                                alt="green iguana"
-                                style={{ objectFit: "fill" }}
-                            />
-                            <CardContent sx={{ p: 0, pr: 2, pl: 2 }}>
-                                <Typography
-                                    gutterBottom
-                                    variant="h5"
-                                    component="div"
-                                >
-                                    {project.title}
-                                </Typography>
-                                <Typography
-                                    variant="body2"
-                                    color="text.secondary"
-                                >
-                                    {project.body}
-                                </Typography>
-                            </CardContent>
-                            <CardActions sx={{ pt: 0 }}>
-                                <Button size="small">
-                                    <a href={project.github}>Github Link</a>
-                                </Button>
-                                <Button size="small">
-                                    <a href={project.deployed}>
-                                        Deployed Application
-                                    </a>
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grow>
+                    <div key={index}>
+                        <Grow
+                            in={true}
+                            timeout={2000}
+                            style={{
+                                transitionDelay: project.delay,
+                            }}
+                        >
+                            <Card sx={{ maxWidth: 300, mb: 1 }}>
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image={project.image}
+                                    alt="green iguana"
+                                    style={{ objectFit: "fill" }}
+                                />
+                                <CardContent sx={{ p: 0, pr: 2, pl: 2 }}>
+                                    <Typography
+                                        gutterBottom
+                                        variant="h5"
+                                        component="div"
+                                    >
+                                        {project.title}
+                                    </Typography>
+                                    <Typography
+                                        variant="body2"
+                                        color="text.secondary"
+                                    >
+                                        {project.body}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions sx={{ pt: 0 }}>
+                                    <Button size="small">
+                                        <a href={project.github}>Github Link</a>
+                                    </Button>
+                                    <Button size="small">
+                                        <a href={project.deployed}>
+                                            Deployed Application
+                                        </a>
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grow>
+                    </div>
                 )
             })}
         </div>
