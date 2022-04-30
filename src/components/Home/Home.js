d909c13f6f54bdf0a2ce37736a7d5b5c83f19157
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-04-30 04:23:47 -0700

    added back to top arrow for home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,26 +2,82 @@ import * as React from "react"
 import Container from "@mui/material/Container"
 import { Hero, BottomNav, AboutText } from "./index"
 import Fade from "@mui/material/Fade"
+import useScrollTrigger from "@mui/material/useScrollTrigger"
+import Fab from "@mui/material/Fab"
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp"
+import Zoom from "@mui/material/Zoom"
+import Grow from "@mui/material/Grow"
+import Box from "@mui/material/Box"
 import ParticleEffect from "./Particles"
 import { Link as ReactLink } from "react-router-dom"
-const Home = () => {
+
+function ScrollTop(props) {
+    const { children, window } = props
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    })
+    return (
+        <Zoom in={trigger}>
+            <Box
+                onClick={handleClick}
+                role="presentation"
+                sx={{ position: "fixed", bottom: 100, right: 10 }}
+            >
+                {children}
+            </Box>
+        </Zoom>
+    )
+}
+
+const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+        "#back-to-top-anchor",
+    )
+    if (anchor) {
+        anchor.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+        })
+    }
+}
+const Home = (props) => {
     const heroStyle = {
         position: "relative",
         zIndex: "0 !important",
     }
     return (
-        <ReactLink to="/" style={{ textDecoration: "none", cursor: "default" }}>
-            <Container>
-                <ParticleEffect />
-                <Fade in={true} timeout={2000}>
-                    <div style={heroStyle}>
-                        <Hero />
-                        <AboutText />
-                        <BottomNav />
-                    </div>
-                </Fade>
-            </Container>
-        </ReactLink>
+        <React.Fragment>
+            <div id="back-to-top-anchor"></div>
+            <ReactLink
+                to="/"
+                style={{ textDecoration: "none", cursor: "default" }}
+            >
+                <Container>
+                    <ParticleEffect />
+                    <Fade in={true} timeout={2000}>
+                        <div style={heroStyle}>
+                            <Hero />
+                            <AboutText />
+                            <BottomNav />
+                        </div>
+                    </Fade>
+                </Container>
+            </ReactLink>
+            <ScrollTop {...props}>
+                <Fab
+                    className={"scrollTop"}
+                    size="small"
+                    aria-label="scroll back to top"
+                >
+                    <KeyboardArrowUpIcon />
+                </Fab>
+            </ScrollTop>
+        </React.Fragment>
     )
 }
 export default Home
